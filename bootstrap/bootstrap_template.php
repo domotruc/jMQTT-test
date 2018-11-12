ab43cd599cb53a0f797e42c24153f2e2f85ffc92
domotruc <domotruc@gmail.com>
2018-11-12 21:52:31 +0100

    Add phpunit.xml and bootstrap template

---
+++ bootstrap/bootstrap_template.php
@@ -0,0 +1,26 @@
+<?php
+
+//
+// PluginTestCase parameters
+//
+$_ENV['jeedom_url'] = 'http://your_jeedom_url/';
+$_ENV['jeedom_username'] = '';
+$_ENV['jeedom_password'] = '';
+
+// Plugin version to install (beta or stable)
+$_ENV['plugin_version'] = 'beta';
+
+
+//
+// jMQTT test case parameters
+//
+
+// broker parameters
+$_ENV['mosquitto_client_id'] = 'jeedom';
+$_ENV['mosquitto_host'] = 'localhost';
+$_ENV['mosquitto_port'] = 1883;
+
+// ssh connexion to jeedom host (to uninstall plugin dependancies)
+$_ENV['ssh_host'] = 'your_jeedom_host';
+$_ENV['ssh_username'] = '';
+$_ENV['ssh_password'] = '';
